36453bdc5e12429ae1d643a1cd8af514a93c7ac0
StanAtilovich <alano92@yandex.ru>
2023-05-09 19:14:59 +0300

    2 задача решение 2

--- src/main/kotlin/lesson_1_task_2/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1_task_2/lesson_1_task_2.kt
@@ -1,11 +1,10 @@
 package lesson_1_task_2
 
-fun main(){
+
+fun main() {
     val numberOfOrders = 75
     val gratitude = "Thanks for $numberOfOrders shopping!\n"
     println(gratitude)
-
-
     val numberOfEmployees = 2000
     println("количество работников в данный момент $numberOfEmployees")
     val numberEmployeesNow = numberOfEmployees - 1
