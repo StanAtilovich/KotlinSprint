a64495782aad831d24cf82fc39756b9dc01a7fbc
StanAtilovich <alano92@yandex.ru>
2023-05-10 18:24:02 +0300

    KS_2_2

---
+++ src/main/kotlin/lesson_2_task_2/lesson_2_task_2.kt
@@ -0,0 +1,11 @@
+package lesson_2_task_2
+
+fun main() {
+    val numberOfOrders = 75
+    val gratitude = "Thanks for $numberOfOrders shopping!"
+    println(gratitude)
+    val numberOfEmployees = 2000
+    println("количество работников в данный момент $numberOfEmployees")
+    val numberEmployeesNow = numberOfEmployees - 1
+    println("1 работник ушел, осталось $numberEmployeesNow")
+}
