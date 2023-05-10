5537257b0f82beef20ccf7de1ff1ed62529aa2ae
StanAtilovich <alano92@yandex.ru>
2023-05-10 18:12:40 +0300

    l1_4 исправил val partOfYear = "2.0547945205479453E-4" т.к это тоже число

--- src/main/kotlin/l1_4/l1_4.kt
+++ src/main/kotlin/l1_4/l1_4.kt
@@ -5,7 +5,7 @@ fun main() {
     val age: Byte = 27
     val partOfDay = 0.075
     val second: Short = 6480
-    val partOfYear = "2.0547945205479453E-4"
+    val partOfYear = 2.0547945205479453E-4
     val apogee = 327000
 
     println("\nДанные для задачи:\nрастояние $lenght\nВозраст - $age\nЧасть дня - $partOfDay\nСекунды$second\nЧасть года - $partOfYear\nАпогей арбиты - $apogee")
