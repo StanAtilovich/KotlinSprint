27dbdb6c41568ce8699fb6e5cb57cf1046ad1ed8
StanAtilovich <alano92@yandex.ru>
2023-05-15 18:46:05 +0300

    KS_2_5

--- src/main/kotlin/lesson_2_task_5/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2_task_5/lesson_2_task_5.kt
@@ -6,7 +6,9 @@ fun main() {
     fun calculateFutureValue(initialDeposit: Int, annualRate: Double, years: Int): Double {
         val rate = 1 + annualRate
         val compoundInterest = rate.pow(years.toDouble())
-        return initialDeposit * compoundInterest    }
+        return initialDeposit * compoundInterest
+    }
+
     val futureValue = calculateFutureValue(70_000, 0.167, 20)
     println(String.format("Ваш вклад через 20 лет составит %.3f рублей", futureValue))
 }
